41c00527e8dbee5d76acfaf609ac74459f4010a8
Kamal Hussein <kamalinuxx@gmail.com>
2022-12-05 12:57:56 -0800

    Adding a script that prepares a MySQL server for the project: A database hbnb_test_db A new user hbnb_test (in localhost) The password of hbnb_test should be set to hbnb_test_pwd hbnb_test should have all privileges on the database hbnb_test_db (and only this database) hbnb_test should have SELECT privilege on the database performance_schema (and only this database) If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- prepares a MySQL server for the project
+-- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
